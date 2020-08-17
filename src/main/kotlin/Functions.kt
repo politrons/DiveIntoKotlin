f9c82e5ba7781417ed7e340293a73366f659cd74
politrons <pablo.perez.garcia@ing.com>
2020-08-17 11:33:08 +0200

    classes

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -13,6 +13,7 @@ fun main() {
     val isMyNameFunc: (String) -> Boolean = outputFunc("username")
     println(isMyNameFunc("politrons"))
     println(inferredFunc(10, "x"))
+    println(compositionResult)
 }
 
 /**
@@ -84,3 +85,12 @@ val outputFunc: (String) -> ((String) -> Boolean) =
  * Also the return type it will be inferred by the compiler
  */
 val inferredFunc = { a: Int, b: String -> b.toUpperCase() + (a * 100) + b.toUpperCase() }
+
+/**
+ * Compose functions in Kotlin can be done just like in any other language, you pass the function as
+ * argument of the one which expect the output of the function and so on.
+ * And finally you need to just invoke the function that start everything.
+ */
+val lowerCaseFunc: (String) -> String = { value -> value.toLowerCase() }
+val appendCharacter: (String, String) -> String = { word, character -> word + character }
+val compositionResult = lowerCaseFunc(appendCharacter(lowerCaseFunc("HELLO"), " KOTLIN"))
