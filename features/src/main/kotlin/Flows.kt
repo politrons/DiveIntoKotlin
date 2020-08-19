a52333834f997ce9fc95e06309194d32e2b4699f
politrons <pablo.perez.garcia@ing.com>
2020-08-20 00:39:13 +0200

    add coroutines

--- features/src/main/kotlin/Flows.kt
+++ features/src/main/kotlin/Flows.kt
@@ -1,7 +1,9 @@
 package main.kotlin
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
 
 /**
@@ -11,11 +13,17 @@ import kotlinx.coroutines.runBlocking
  * To pass the emission from the stream to the subscriber, the flow ust use operator [emit]
  * In order to make it eager or subscribe, you need to use [collect]
  */
+@ExperimentalCoroutinesApi
 @FlowPreview
 fun main() {
     runBlocking { simpleFlow() }
     runBlocking { streamFlows() }
     runBlocking { flatMapFlows() }
+    runBlocking { transform() }
+    runBlocking { reduce() }
+    runBlocking { filter() }
+    runBlocking { scan() }
+    runBlocking { bulkHeadPattern() }
 }
 
 /**
@@ -38,7 +46,8 @@ private suspend fun simpleFlow() {
  * As monads [map] operator to transform the data emitted is allowed.
  */
 private suspend fun streamFlows() {
-    (1..5).asFlow()
+    (1..5)
+        .asFlow()
         .map { number -> number * 10 }
         .collect { result -> println(result) }
 }
@@ -49,7 +58,73 @@ private suspend fun streamFlows() {
  */
 @FlowPreview
 private suspend fun flatMapFlows() {
-    listOf("hello", "composition", "flow", "world").asFlow()
+    listOf("hello", "composition", "flow", "world")
+        .asFlow()
         .flatMapMerge { number -> flow { emit(number.toUpperCase()) } }
         .collect { result -> println(result) }
+}
+
+/**
+ * Transform operator allow receive the data, and emit as much data as we want using [emit] operator.
+ */
+private suspend fun transform() {
+    listOf("hello", "transform", "flow", "world")
+        .asFlow()
+        .transform { value ->
+            emit(value)
+            emit("$value _")
+            emit("$value __")
+        }
+        .collect { result -> println(result) }
+}
+
+/**
+ * Using reduce operator it will pass execute a function where it will receive the accumulator value, and
+ * the next element of the array. Just like fold, but here the accumulator type is always the same than element
+ * you have in the iterator
+ */
+private suspend fun reduce() {
+    val sum = listOf(1, 2, 3, 4, 5)
+        .asFlow()
+        .reduce { acc, next -> acc + next }
+    println(sum)
+}
+
+/**
+ * Regular filter operators like in all reactive streams frameworks.
+ */
+private suspend fun filter() {
+    listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        .asFlow()
+        .filter { value -> value > 3 }
+        .takeWhile { value -> value < 7 }
+        .collect { number -> println(number) }
+}
+
+/**
+ * [Scan] operator is the [fold] in the reactive stream world. It works exactly in the same way, you declare
+ * as first argument the accumulator type, and the second argument is a function where you receive the
+ * accumulator type defined previously and the next element of the iterator
+ */
+@ExperimentalCoroutinesApi
+private suspend fun scan() {
+    val numbers =
+        listOf(1, 2, 3, 4, 5)
+            .asFlow()
+            .scan(emptyList<Int>()) { acc, value -> acc + value }
+            .toList()
+            .flatten()
+    println(numbers)
+}
+
+/**
+ * Using [flowOn] operator, just like [subscribeOn] of Rx we are able to configure in which dispatcher we want to
+ * execute the stream, setting a maximum resources(Threads) that the stream can consume.
+ */
+private suspend fun bulkHeadPattern() {
+    listOf("hello", "dispatcher", "flow", "world")
+        .asFlow()
+        .onEach { result -> println("Element $result running in thread ${Thread.currentThread().name}") }
+        .flowOn(context = newFixedThreadPoolContext(10, "MyLightweightThread"))
+        .collect { result -> println("Final result $result in thread ${Thread.currentThread().name}") }
 }
