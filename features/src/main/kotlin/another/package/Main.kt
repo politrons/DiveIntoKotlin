ad2bbcfe7e18a057b90163ba3d593b85a3cca9c7
politrons <politrons@gmail.com>
2020-08-21 22:59:37 +0200

    add Channels.kt

--- features/src/main/kotlin/another/package/Main.kt
+++ features/src/main/kotlin/another/package/Main.kt
@@ -10,7 +10,6 @@ import main.kotlin.simpleChannel
  * Kotlin just allow run files without being in a class, just like Golang or Haskell,
  * You just need to create a file ended with <kt> extension.
  * To create a runner in Kotlin you just need to create main() method.
- *
  * You can also invoke functions from another files just importing the function
  */
 fun main() {
