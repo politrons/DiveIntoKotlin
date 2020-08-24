c5adaf51d97fbf5f65dddf0059f7ca4650a6f02f
politrons <politrons@gmail.com>
2020-08-24 11:54:39 +0200

    add ktor

--- features/src/main/kotlin/arrow/FunctionalEffects.kt
+++ features/src/main/kotlin/arrow/FunctionalEffects.kt
@@ -1,26 +1,54 @@
 package main.kotlin.arrow
 
+import arrow.core.Either
+import arrow.core.getOrElse
+import arrow.core.right
 import arrow.fx.IO
 import arrow.fx.extensions.fx
+import arrow.fx.extensions.toIO
 import arrow.fx.handleError
-import arrow.unsafe
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 fun main() {
+    iOSuccessProgram()
+    iOErrorProgram()
+    functionalEffectProgram()
+}
+
+private fun iOSuccessProgram() {
     val program: IO<String> = IO.fx {
         val effect1 = !IO.effect { asyncValue1() }
         val effect2 = !IO.effect { asyncValue2() }
         "$effect1 $effect2"
     }
     println(program.unsafeRunSync())
+}
 
+private fun iOErrorProgram() {
     val programError: IO<String> = IO.fx {
         val effect1 = !IO.effect { asyncValue1() }
         val effect2 = !IO.effect { throw NullPointerException() }
         "$effect1 $effect2"
     }.handleError { t -> "Error Handler on program. Caused by $t" }
     println(programError.unsafeRunSync())
+}
+
+fun functionalEffectProgram() {
+    val functionalEffectsProgram: IO<String> = IO.fx {
+        val optionValue = !IO.effect { maybeString("hello pure").getOrElse { throw IllegalStateException() } }
+        val eitherValue = when (val eitherValue =
+            !eitherString("functional effect world").right().toIO()) {
+            is Either.Right -> eitherValue.b
+            is Either.Left -> throw IllegalStateException()
+        }
+        "$optionValue - $eitherValue"
+    }.handleError { t -> "Error Handler on functional effect program. Caused by $t" }
+    println(functionalEffectsProgram.unsafeRunSync())
+}
+
+fun interopOption(){
+
 }
 
 private suspend fun asyncValue2() = withContext(Dispatchers.Default) {
@@ -30,3 +58,4 @@ private suspend fun asyncValue2() = withContext(Dispatchers.Default) {
 private suspend fun asyncValue1() = withContext(Dispatchers.Default) {
     "hello pure"
 }
+
---
+++ features/src/main/kotlin/ktor/Server.kt
@@ -0,0 +1,22 @@
+package main.kotlin.ktor
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    val server: NettyApplicationEngine = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
