be60470ab9959c8bb20c0fa44599df0c1c6081a4
politrons <politrons@gmail.com>
2021-04-25 20:19:51 +0200

    Add subscriber pattern

---
+++ features/src/main/kotlin/patterns/PublisherClass.kt
@@ -0,0 +1,65 @@
+package main.kotlin.patterns
+
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Runner of the pattern, where we create an instance [PublisherClass].
+ * Once we create several subscribers for the example, the publisher invoke all of them.
+ */
+fun main() {
+    val publisher = PublisherClass()
+    publisher.addSubscriber(1, StringSubscriber())
+    publisher.addSubscriber(2, IntSubscriber())
+
+    publisher.invokeSubscriber(1, "Something happens wake up")
+    publisher.invokeSubscriber(2, 1981)
+
+    //Wait for Async process to finish
+    Thread.sleep(1000)
+}
+
+/**
+ * The Publisher class contains the [addSubscriber] where we add the subscriber with the id.
+ * Since [invokeSubscriber] is an async operation, we have top wrap into [runBlocking]
+ * We get the subscriber using the id, and using [?] elvis operator, we're able to control the null elements of map.
+ * Once we have the subscriber we invoke the interface function and we pass the generic data.
+ */
+class PublisherClass {
+    private val subscribers: MutableMap<Int, SubscriberClass> = mutableMapOf()
+
+    fun addSubscriber(id: Int, subscriberClass: SubscriberClass) {
+        subscribers[id] = subscriberClass
+    }
+
+    fun <T> invokeSubscriber(id: Int, data: T) {
+        runBlocking {
+            subscribers[id]?.invoke(data)
+        }
+    }
+
+}
+
+/**
+ * In this class we define the interface [SubscriberClass] that have thr generic [invoke] function.
+ * We create [StringSubscriber] and also [IntSubscriber] that implement [invoke]
+ */
+interface SubscriberClass {
+    fun <T> invoke(data: T)
+}
+
+class StringSubscriber : SubscriberClass {
+    override fun <String> invoke(data: String) {
+        println("Data:$data")
+        println("Doing some logic in String subscriber")
+    }
+}
+
+class IntSubscriber : SubscriberClass {
+    override fun <Int> invoke(data: Int) {
+        println("Data:$data")
+        println("Doing some logic in Int subscriber")
+    }
+}
+
+
+
